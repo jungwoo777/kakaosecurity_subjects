79d13c3b7ec5b900adb32238c171ab3a9082a849
jungwoo777 <110328524+jungwoo777@users.noreply.github.com>
2022-08-02 22:21:05 +0900

    메인 주석 추가

--- kakaosecurity/src/main/java/com/example/kakaosecurity/KakaosecurityApplication.java
+++ kakaosecurity/src/main/java/com/example/kakaosecurity/KakaosecurityApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 메인 함수
+ * 
+ * 로컬로 실행 후 
+ * http://localhost:8080/swagger-ui/index.html#/
+ * 브라우저에 위 주소로 된 swagger ui를 실행하여 수행
+ * 
+ * h2는 in-memory방식으로 구성
+ * 프로젝트 시작과 동시에 초기 데이터 구성
+ * 
  * @author jungwoo
  *
  */
